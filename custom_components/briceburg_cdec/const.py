7c5bd38277e3bb4a6bf15c34119058590bd08d85
peluke <peluke@gmail.com>
2026-08-22 12:11:48 -0700

    Update Home Assistant CDEC naming and station setup

--- custom_components/briceburg_cdec/const.py
+++ custom_components/briceburg_cdec/const.py
@@ -3,11 +3,12 @@
 from datetime import timedelta
 
 DOMAIN = "briceburg_cdec"
-NAME = "Briceburg CDEC"
+NAME = "Home Assistant CDEC"
 VERSION = "0.1.0"
 DEFAULT_STATION = "MBG"
 DEFAULT_SCAN_INTERVAL = timedelta(minutes=15)
 REPORT_URL = "https://cdec.water.ca.gov/dynamicapp/QueryF"
+JSON_URL = "https://cdec.water.ca.gov/dynamicapp/req/JSONDataServlet"
 CONF_STATION = "station"
 CONF_SENSOR_NUM = "sensor_num"
 DEFAULT_SENSOR_NUM = "20,25,4"
